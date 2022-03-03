ba678d2cb45e10dfcb0624e9d3bf9002cc6712b3
TerentevVlad <ter.vlad.ter@gmail.com>
2022-03-03 12:23:09 +0300

    Добавил SceneDescription

---
+++ Runtime/Description/IScenesConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace LittleBit.Modules.SceneLoader.Description
+{
+    public interface IScenesConfig
+    {
+        public SceneDescription LoadingScreenScene { get; }
+        public IReadOnlyList<SceneDescription> QueueScenes { get; }
+        public float MinLogoDuration { get; }
+    }
+}
---
+++ Runtime/Description/SceneDescription.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace LittleBit.Modules.SceneLoader.Description
+{
+    [CreateAssetMenu(fileName = "DescriptionScene", menuName = "Configs/Scenes/DescriptionScene", order = 0)]
+    public class SceneDescription : ScriptableObject
+    {
+        [SerializeField] private string nameScene;
+        [SerializeField] private SceneReference sceneReference;
+
+        public string NameScene => nameScene;
+        public SceneReference SceneReference => sceneReference;
+    }
+}
